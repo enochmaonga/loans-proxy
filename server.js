2c9eed22fec620c855459e4ebec8dbd4a4a2c3e2
Enoch Maonga Mokua <emokua@safaricom.co.ke>
2024-08-19 15:21:45 +0300

    adding firebase Storage, fix

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const { MongoClient } = require("mongodb");
 const bodyParser = require('body-parser');
 const path = require('path');
 const admin = require('firebase-admin');
+console.log('Service Account Path:', process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
+console.log('Service Account JSON:', serviceAccount);
 
 // Load service account key file
 // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
